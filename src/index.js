881dde1156cee71d57b4ebf93adfb67b3fcb9fc7
Md. Rony <rony15-10527@diu.edu.bd>
2021-12-01 16:08:40 +0600

    Creating react app called my-app

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>Hello, React!</h1>,
   document.getElementById('root')
 );
 
